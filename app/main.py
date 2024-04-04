c8446ca3d10b5dd6ee562bfb923295c24b4544f8
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 22:27:38 +0800

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,24 +1,36 @@
-# Uncomment this to pass the first stage
 import socket
 
-HOST = "localhost"
-PORT = 4221
 
-print(f"Server started at: http://{HOST}:{PORT}")
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    connection = server_socket.accept()[0]  # wait for client
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+    request = connection.recv(4096)
+    request = request.decode()
+    print(request)
 
-    conn, addr = s.accept()
+    path_start = request.find("/")
+    path_end = request.find(" ", path_start)
+    path = request[path_start:path_end]
 
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
+    file_start = path.rfind("/") + 1
+    file_name = path[file_start:]
+    
+    path = path.removesuffix(file_name)
 
-            if not data:
-                break
+    print(path, "\n")
+    print(file_name, "\n\n")
 
-            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-            conn.sendall(data)
+    if path == "/":
+        msg = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path == "/echo/":
+        msg = "HTTP/1.1 200 OK\r\n"
+        msg += "Content-Type: text/plain\r\n"
+        msg += "Content-Length: ", len(file_name), "\r\n"
+    else:
+        msg = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    connection.send(msg.encode())
+
+    if __name__ == "__main__":
+        main()
