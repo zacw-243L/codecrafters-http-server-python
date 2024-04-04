064caf21b54b4ee738e7cff31df20a42b1761eea
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 22:22:02 +0800

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,20 +1,24 @@
 # Uncomment this to pass the first stage
 import socket
-def main():
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
-    print("listening on port 4221:")
-    conn, addr = server_socket.accept()
+
+HOST = "localhost"
+PORT = 4221
+
+print(f"Server started at: http://{HOST}:{PORT}")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+
+    conn, addr = s.accept()
+
     with conn:
+        print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+
             if not data:
                 break
-            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-1
-            conn.sendall(data)
-if __name__ == "__main__":
-    main()
 
+            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.sendall(data)
