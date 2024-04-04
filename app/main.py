4cf3e05d67093e92c4caef3680ae2b2330e55d52
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 22:35:46 +0800

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,36 +1,38 @@
 import socket
+from typing import List
+
+
+def parse_input(input: bytes) -> List[str]:
+    input = input.decode()
+    return input.split("\r\n")
+
+
+def handle_request(input: bytes) -> bytes:
+    # we expect requests like this:
+    # GET /index.html HTTP/1.1
+    # Host: localhost:4221
+    # User-Agent: curl/7.64.1
+    input = parse_input(input)
+    method, path, version = input[0].split(" ")
+    output = ""
+    if path == "/":
+        output = b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        output = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    return output
 
 
 def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection = server_socket.accept()[0]  # wait for client
+    connection, client = server_socket.accept()  # wait for
 
-    request = connection.recv(4096)
-    request = request.decode()
-    print(request)
+    with connection:
+        input = connection.recv(1024)
+        connection.sendall(handle_request(input))
+    server_socket.close()
 
-    path_start = request.find("/")
-    path_end = request.find(" ", path_start)
-    path = request[path_start:path_end]
 
-    file_start = path.rfind("/") + 1
-    file_name = path[file_start:]
-    
-    path = path.removesuffix(file_name)
-
-    print(path, "\n")
-    print(file_name, "\n\n")
-
-    if path == "/":
-        msg = "HTTP/1.1 200 OK\r\n\r\n"
-    elif path == "/echo/":
-        msg = "HTTP/1.1 200 OK\r\n"
-        msg += "Content-Type: text/plain\r\n"
-        msg += "Content-Length: ", len(file_name), "\r\n"
-    else:
-        msg = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-    connection.send(msg.encode())
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
