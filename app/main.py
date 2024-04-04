a7495cdfdc490cfa70841a133aea6e00c7494bad
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 22:51:53 +0800

    kill me

--- app/main.py
+++ app/main.py
@@ -1,38 +1,34 @@
 import socket
-from typing import List
-
-
-def parse_input(input: bytes) -> List[str]:
-    input = input.decode()
-    return input.split("\r\n")
-
-
-def handle_request(input: bytes) -> bytes:
-    # we expect requests like this:
-    # GET /index.html HTTP/1.1
-    # Host: localhost:4221
-    # User-Agent: curl/7.64.1
-    input = parse_input(input)
-    method, path, version = input[0].split(" ")
-    output = ""
-    if path == "/":
-        output = b"HTTP/1.1 200 OK\r\n\r\n"
-    else:
-        output = b"HTTP/1.1 404 Not Found\r\n\r\n"
-    return output
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+
     print("Logs from your program will appear here!")
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, client = server_socket.accept()  # wait for
+    connection, address = server_socket.accept()
 
     with connection:
-        input = connection.recv(1024)
-        connection.sendall(handle_request(input))
-    server_socket.close()
+        print("Connected by", address)
+        # infinite loop to keep the connection open and receive data continuously
+        while True:
+            request = connection.recv(1024)
+            req_str = request.decode("utf-8")
+            splits = req_str.split(" ")
+            method = splits[0]
+            path = splits[1]
 
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8")
 
+            if path == "/":
+                response = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+            elif "/echo/" in path:
+                data = path.split("/echo/")[1]
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode("utf-8")
+
+            connection.sendall(response)
+
+            
 if __name__ == "__main__":
     main()
+
