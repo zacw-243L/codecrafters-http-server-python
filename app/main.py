13a3030464bf3a3eb7b2cad9f3c21f9e13d9ffe2
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 22:03:00 +0800

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import socket
 
 
 def main():
-    You can use print statements as follows for debugging, they'll be visible when running tests.
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
