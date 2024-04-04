77f7d603eabc9e17e274b436111888a3040f6376
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-04 23:13:17 +0800

    mass murder

--- app/main.py
+++ app/main.py
@@ -1,66 +1,64 @@
 import socket
-import threading
-
-HTTP_SUCCESS_RESPONSE = "HTTP/1.1 200 OK\r\n"
-HTTP_NOT_FOUND_RESPONSE = "HTTP/1.1 404 NOT-FOUND\r\n\r\n"
+from threading import Thread
 
 
-def formatResponse(request_params):
-    response = []
-    for param in range(2, len(request_params)):
-        response.append(request_params[param])
+def get_user_agent(request):
+    lines = request.split("\r\n")
+    for line in lines:
+        if "User-Agent" in line:
+            return line.split("User-Agent: ")[1]
+    return "User-Agent not found"
 
-        if param != len(request_params) - 1:
-            response.append("/")
-    return "".join(response)
+
+def get_host(request):
+    lines = request.split("\r\n")
+    for line in lines:
+        if "Host" in line:
+            return line.split("Host: ")[1]
+    return "Host not found"
+
+
+def get_req(request):
+    lines = request.split("\r\n")
+    method, path, protocol = lines[0].split(" ")
+    return method, path, protocol
+
+
+def handle_connection(connection, address):
+    with connection:
+        print("Connected by", address)
+        # infinite loop to keep the connection open and receive data continuously
+        while True:
+            request = connection.recv(1024)
+            req_str = request.decode("utf-8")
+            method, path, protocol = get_req(req_str)
+            host = get_host(req_str)
+            agent = get_user_agent(req_str)
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8")
+            if path == "/":
+                response = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+            elif "/user-agent" in path:
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(agent)}\r\n\r\n{agent}".encode(
+                    "utf-8"
+                )
+            elif "/echo/" in path:
+                data = path.split("/echo/")[1]
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode(
+                    "utf-8"
+                )
+
+            connection.sendall(response)
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
     while True:
-        connection = server_socket.accept()
-        thread_connection = threading.Thread(target=handler, args=(connection,), daemon=True).start()
-
-
-def handler(connection):
-    data = connection[0].recv(1024)
-    request = data.decode()
-
-    request_lines = request.split("\r\n")
-    method, route, http_version = request_lines[0].split()
-    host = request_lines[1].split()
-
-    request_params = route.split("/")
-
-    if request_params[1] == "echo":
-        body = formatResponse(request_params)
-        response = (HTTP_SUCCESS_RESPONSE + "Content-Type: text/plain\r\nContent-Length: " + str(len(body)) + "\r\n\r\n" + body)
-        connection[0].send(response.encode())
-
-        if request_params[1] == "user-agent":
-            user_agent = request_lines[2].split()[1]
-            body = formatResponse(request_params)
-            response = (
-                    HTTP_SUCCESS_RESPONSE
-                    + "Content-Type: text/plain\r\nContent-Length: "
-                    + str(len(user_agent))
-                    + "\r\n\r\n"
-                    + str(user_agent)
-            )
-            print(response)
-            connection[0].send(response.encode())
-
-        if route != "/":
-            connection[0].send(HTTP_NOT_FOUND_RESPONSE.encode())
-
-        else:
-            response = HTTP_SUCCESS_RESPONSE + "\r\n"
-            connection[0].send(response.encode())
-        connection[0].close()
+        connection, address = server_socket.accept()
+        thread = Thread(target=handle_connection, args=(connection, address))
+        thread.start()
 
 
 if __name__ == "__main__":
     main()
+    
